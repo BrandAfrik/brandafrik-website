7f31f54dc53339cc55a347c01d56028537039029
AmosAidoo <aidooamos20@gmail.com>
2019-06-15 01:34:31 +0000

    The dashboard UI

---
+++ admin/dashboard.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include_once '../includes/head_items.php'?>
+    <link rel="stylesheet" type="text/css" href="styles/admin-style.css"/>
+    <title>Admin</title>
+</head>
+<body>
+    <div class="main-container">
+        <div class="admin-left-sidebar">
+            
+        </div>
+
+        <div class="admin-content-area">
+            <header class="admin-header" class="pl-3">
+                <h4>Dashboard</h4>
+            </header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h4 class="text-center">Users</h4>
+                    </div>
+
+                    <div class="col-md-4">
+                        <h4 class="text-center">Posts</h4>
+                    </div>
+
+                    <div class="col-md-4">
+                        <h4 class="text-center">Others</h4>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    
+</body>
+</html>
