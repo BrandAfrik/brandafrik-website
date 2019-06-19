e3085accb9cb9541669608baeb388f8d2e188206
AmosAidoo <aidooamos20@gmail.com>
2019-06-19 21:56:40 +0000

    The user details to the right top corner

---
+++ admin/admin_includes/right_user_details.php
@@ -0,0 +1,4 @@
+<div style="float:right">
+    <a href="#"><div class="bg-light" style="width:30px; height:30px; display:inline-block"></div><a>
+    <span style="position:relative; top:-10px">User</span>
+</div>
