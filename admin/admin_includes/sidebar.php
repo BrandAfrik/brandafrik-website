cae1bc255ea62cb79072bf3a0e292fa7214cbad9
AmosAidoo <aidooamos20@gmail.com>
2019-06-19 21:53:13 +0000

    I've moved the fixed left sidebar into a separate file

---
+++ admin/admin_includes/sidebar.php
@@ -0,0 +1,21 @@
+<div class="admin-left-sidebar text-white">
+    <div class="p-2 border-bottom border-success">
+       <h5 style="font-family:airstrip">Brand Afrik</h5> 
+    </div>
+
+    <div class="admin-left-sidebar-menu p-2">
+        <h5 class="pl-3">Menu</h5>
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link text-white" href="dashboard.php">Dashboard</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="posts.php">Posts</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="users.php">Users</a>
+            </li>
+        </ul>
+    </div>
+    
+</div>
