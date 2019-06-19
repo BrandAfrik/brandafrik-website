6de9331318cdfdbf4368aa81ae9aba36c1b8e05b
AmosAidoo <aidooamos20@gmail.com>
2019-06-19 22:12:39 +0000

    I've added logout dropdown

--- admin/admin_includes/right_user_details.php
+++ admin/admin_includes/right_user_details.php
@@ -1,4 +1,7 @@
 <div style="float:right">
-    <a href="#"><div class="bg-light" style="width:30px; height:30px; display:inline-block"></div><a>
+    <a href="#" data-toggle="dropdown"><div class="bg-light" style="width:30px; height:30px; display:inline-block"></div><a>
     <span style="position:relative; top:-10px">User</span>
+    <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Logout</a>
+    </div>
 </div>
